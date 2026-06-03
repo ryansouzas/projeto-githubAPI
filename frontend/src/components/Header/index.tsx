140f46b891f0b689557622f9856ee160886f757d
Ryan Souza <ryanssebadelhe@gmail.com>
2026-06-03 13:42:44 -0300

    Header Component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import "./styles.css";
+
 export default function Header() {
     return (
-        <header>
+        <header className="app-header">
             <h2>Github API</h2>
         </header>
     );
